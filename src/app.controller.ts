9e94e5130f76ab9b429f8f36211ed5ebdd3fc97b
shp7408 <shp7408@naver.com>
2023-09-17 22:40:59 +0900

    로컬 mysql, typeorm 세팅, fastify 세팅

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,41 +1,14 @@
-import { TypedBody, TypedParam, TypedRoute } from "@nestia/core";
-import { Controller } from "@nestjs/common";
+import { Controller, Get, Req } from "@nestjs/common";
 import { AppService } from "./app.service";
+import { RouteConfig } from "@nestjs/platform-fastify";
 
-interface TestDto {
-  /**
-   * @format email
-   */
-  email: string;
-}
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) { }
 
-
-  @TypedRoute.Get()
-  getHelloWorld(): string {
-    console.log("work...");
-    return this.appService.getHello();
-  }
-
-  @TypedRoute.Post("nestia")
-  getHello(@TypedBody() dto: TestDto): string {
-    console.log("work...");
-    return dto.email;
-  }
-
-  /**
-   * it's very important API.
-   *
-   * how to use? read bellow!
-   *
-   * @param id maybe you know already
-   * @returns true, just return true. that's all.
-   */
-  @TypedRoute.Post(":id")
-  async nestiaTest(@TypedParam("id") id: number): Promise<boolean> {
-    console.log("id : ", id);
-    return true;
+  @RouteConfig({ output: 'hello world' })
+  @Get()
+  index(@Req() req) {
+    return req.routeConfig.output;
   }
 }
