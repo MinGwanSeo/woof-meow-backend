93cccaa13387c784af28a6b270731a37daa4193b
shp7408 <shp7408@naver.com>
2023-09-17 20:16:40 +0900

    nestia 세팅 완료... 물론 완전 기본적인것만...

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,41 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { TypedBody, TypedParam, TypedRoute } from "@nestia/core";
+import { Controller } from "@nestjs/common";
+import { AppService } from "./app.service";
 
+interface TestDto {
+  /**
+   * @format email
+   */
+  email: string;
+}
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
-  @Get()
-  getHello(): string {
+
+  @TypedRoute.Get()
+  getHelloWorld(): string {
+    console.log("work...");
     return this.appService.getHello();
+  }
+
+  @TypedRoute.Post("nestia")
+  getHello(@TypedBody() dto: TestDto): string {
+    console.log("work...");
+    return dto.email;
+  }
+
+  /**
+   * it's very important API.
+   *
+   * how to use? read bellow!
+   *
+   * @param id maybe you know already
+   * @returns true, just return true. that's all.
+   */
+  @TypedRoute.Post(":id")
+  async nestiaTest(@TypedParam("id") id: number): Promise<boolean> {
+    console.log("id : ", id);
+    return true;
   }
 }
